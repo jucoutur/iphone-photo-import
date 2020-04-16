a558830164c95b60542a709589ed0ea7c471a256
jucoutur <julien.coutur@gmail.com>
2020-04-16 14:50:00 +0200

    Add files via upload
    Moved comments to english

--- AAE-suppr.sh
+++ AAE-suppr.sh
@@ -1,19 +1,19 @@
 for f in *.AAE
 do
-	# suppression du fichier sidecar .AAE
+	# remove .AAE sidecar
 	rm "$f"
 	jpg=${f/.AAE/.JPG}
 	png=${f/.AAE/.PNG}
 	mov=${f/.AAE/.MOV}
 	ejpg="${f:0:4}"E"${f:4:4}.JPG"
 	emov="${f:0:4}"E"${f:4:4}.MOV"
-	# Si JPG custom + JPG original => suppr du JPG original + renommer le JPG custom
+	# If both modifed JPG AND original JPG exist => remove original JPG + rename modified JPG
 	test -f "$ejpg" -a -f "$jpg" && rm "$jpg" && mv "$ejpg" "$jpg"
-	# Si JPG custom + PNG (capture d'ecran) => suppr du PNG + renommer le JPG custom
+	# If both modifed JPG AND original PNG (screenshot) exist => remove original PNG + rename modified JPG
 	test -f "$ejpg" -a -f "$png" && rm "$png" && mv "$ejpg" "$jpg"
-	# Si MOV custom + MOV original => suppr du MOV original + renommer le MOV custom
-	test -f "$emov" -a -f "$mov" && rm "$mov" && mv "$emov" "$mov"
-	# Si JPG custom et pas de JPG ou PNG original => uniquement renommer le JPG custom
+	# If modifed JPG exists AND neither original not PNG file exist => rename modified JPG only
 	test -f "$ejpg" -a ! -f "$jpg" -a ! -f "$png" && mv "$ejpg" "$jpg"
+	# If both modified MOV AND original MOV exist => remove original MOV + rename modified MOV
+	test -f "$emov" -a -f "$mov" && rm "$mov" && mv "$emov" "$mov"
 done
-echo "-- Traitement des photos terminé --"
+echo "-- OK : files have been processed --"
